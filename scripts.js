1f1840597583d3d5a83f60dcaede5df0abd8151b
Student <example@example.com>
2019-09-23 16:10:16 -0700

    single page redux

---
+++ scripts.js
@@ -0,0 +1,50 @@
+// LYRIC INFO
+const songLyricsArray = "Don't want to be a fool for you, Just another player in your game for two, You may hate me but it ain't no lie, Baby bye bye bye, Bye bye, I Don't want to make it tough, I just want to tell you that I've had enough, It might sound crazy but it ain't no lie, Baby bye bye bye".split(', ');
+
+
+
+// INITIAL REDUX STATE
+const initialState = {
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 0,
+}
+
+const { createStore } = Redux;
+const store = createStore(reducer);
+console.log(store.getState());
+
+console.log(initialState);
+
+// JEST TESTS + SETUP
+const { expect } = window;
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'NEXT_LYRIC':
+      let newArrayPosition = state.arrayPosition + 1;
+      let newState = {
+        songLyricsArray: state.songLyricsArray,
+        arrayPosition: newArrayPosition,
+      }
+      return newState;
+    default:
+      return state;
+  }
+}
+
+expect(reducer(initialState, { type: null })).toEqual(initialState);
+
+expect(reducer(initialState, { type: 'NEXT_LYRIC' })).toEqual({
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 1
+});
+
+// REDUX STORE
+
+
+// CLICK LISTENER
+const userClick = () => {
+  store.dispatch({ type: 'NEXT_LYRIC' });
+  console.log(store.getState());
+}
+
